dcd569bfcd3d117eda8035a7fffb53981007af00
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2025-07-18 21:33:51 +0530

    completed the sidebar component frontend

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,15 +6,19 @@ import RightSiderbar from '../components/RightSiderbar'
 
 const HomePage = () => {
 
-    const [selectedUser,setselectedUser]=useState(false)
+  const [selectedUser, setselectedUser] = useState(false)
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:gird-cols-[1fr_1.5fr_1fr] xl:grid-cols[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser
+          ? 'md:[grid-template-columns:1fr_1.5fr_1fr] xl:[grid-template-columns:1fr_2fr_1fr]'
+          : 'md:grid-cols-2'
+        }`}>
 
-        <Sidebar/>
-        <ChatContainer/>
-        <RightSiderbar/>
+
+        <Sidebar selectedUser={selectedUser} setselectedUser={setselectedUser} />
+        <ChatContainer selectedUser={selectedUser} setselectedUser={setselectedUser} />
+        <RightSiderbar selectedUser={selectedUser} setselectedUser={setselectedUser} />
       </div>
     </div>
   )
